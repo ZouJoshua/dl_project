727496961f8d2ab63cba37fa9674794d516268b6
zs <joshua_zou@163.com>
2019-05-29 17:57:48 +0800

    update project

--- hi_news_classification/fasttext_predict.py
+++ hi_news_classification/fasttext_predict.py
@@ -13,48 +13,74 @@
 
 import tensorflow as tf
 import numpy as np
-from tf_model.fasttext_model import fastText
-from tflearn.data_utils import pad_sequences
 import os
-import codecs
-from preprocess.preprocess_data_hi import DataSet
-
+import sys
 
 current_work_dir = os.path.realpath(__file__)
 root_dir = os.path.dirname(os.path.dirname(current_work_dir))
+sys.path.append(root_dir)
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from tf_model.fasttext_model import FastText
+from tflearn.data_utils import pad_sequences
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
+from gensim.models import KeyedVectors
+from preprocess.preprocess_data_hi import DataSet
+
+
 data_dir = os.path.join(root_dir, "data", "hi_news")
 training_data_file = os.path.join(data_dir, "top_category_corpus")
 word2vec_file = os.path.join(data_dir, "word2vec.bin")
 model_checkpoint = os.path.join(data_dir, "fasttext_checkpoint")
-model_saved = os.path.join(data_dir, "fasttext_model_saved")
+model_saved = os.path.join(data_dir, "pb_model")
 cache_file_h5py = os.path.join(data_dir, "data.h5")
 cache_file_pickle = os.path.join(data_dir, "vocab_label.pik")
 output_dir = os.path.join(data_dir, "summarys")
 
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-FLAGS=tf.flags.FLAGS
+
+FLAGS = tf.flags.FLAGS
+tf.flags.DEFINE_string("cache_file_h5py", cache_file_h5py, "path of training/validation/test data.")
+tf.flags.DEFINE_string("cache_file_pickle", cache_file_pickle, "path of vocabulary and label files")
+
 tf.flags.DEFINE_integer("label_size", 9, "number of label")
 tf.flags.DEFINE_float("learning_rate", 0.01, "learning rate")
-tf.flags.DEFINE_integer("batch_size", 512, "Batch size for training/evaluating.")  # 批处理的大小 32-->128
-tf.flags.DEFINE_integer("decay_steps", 5000, "how many steps before decay learning rate.")  # 批处理的大小 32-->128
-tf.flags.DEFINE_float("decay_rate", 0.5, "Rate of decay for learning rate.")
-tf.flags.DEFINE_integer("num_sampled", 10, "number of noise sampling")
-tf.flags.DEFINE_string("ckpt_dir", "data/news_classification/fasttext/fasttext_checkpoint/", "checkpoint location for the model")
-tf.flags.DEFINE_integer("title_len", 300, "max title length")
-tf.flags.DEFINE_integer("embed_size", 100, "embedding size")
-tf.flags.DEFINE_boolean("is_training", False, "is traning.true:tranining,false:testing/inference")
-tf.flags.DEFINE_integer("num_epochs", 15, "embedding size")
-tf.flags.DEFINE_integer("validate_every", 3, "Validate every validate_every epochs.")  # 每3轮做一次验证
-tf.flags.DEFINE_string("predict_target_file", "data/news_classification/fasttext/fasttext_checkpoint/predict_target", "target file path for final prediction")
-tf.flags.DEFINE_string("predict_source_file", 'data/news_classification/fasttext/fasttext_checkpoint/predict_source', "target file path for final prediction")
+tf.flags.DEFINE_integer("batch_size", 256, "batch size for training/evaluating")  # 批处理的大小 32-->128
+tf.flags.DEFINE_integer("decay_steps", 10000, "how many steps before decay learning rate")
+tf.flags.DEFINE_float("decay_rate", 0.96, "Rate of decay for learning rate")  # 一次衰减多少
+tf.flags.DEFINE_integer("num_sampled", 100, "number of noise sampling")
+tf.flags.DEFINE_float("dropout_keep_prob", 0.8, "to control the activation level of neurons")
+tf.flags.DEFINE_string("ckpt_dir", model_checkpoint, "checkpoint location for the model")
+tf.flags.DEFINE_integer("sentence_len", 200, "max sentence length")
+tf.flags.DEFINE_integer("embed_size", 300, "embedding size")
+tf.flags.DEFINE_boolean("is_training", False, "true:training, false:testing/inference")
+tf.flags.DEFINE_integer("num_epochs", 20, "epoch times")
+tf.flags.DEFINE_integer("validate_every", 1, "validate every validate_every epochs")  # 每1轮做一次验证
+tf.flags.DEFINE_boolean("use_embedding", True, "whether to use embedding or not")
 
 
 def main(_):
     """导入数据 -> 创建session -> 喂数据 -> 训练 -> (验证) ->（预测）"""
-    ds = DataSet(data_dir, word2vec_file, training_data_file, embedding_dims=FLAGS.embed_size)
 
+    # step1 -> load data
+    ds = DataSet(data_dir, word2vec_file, training_data_file, embedding_dims=FLAGS.embed_size)
+    train, test, _ = ds.load_data(use_embedding=True, valid_portion=0.2)
+    index2label = ds.index2label
+    vocab_embedding = ds.embedding
     vocab_size = len(ds.word2index)
     print("fasttext_model.vocab_size:", vocab_size)
+    # num_classes = len(ds.label2index)
+    # print("num_classes:", num_classes)
+    print("num_classes:", FLAGS.label_size)
+    trainX, trainY = train
+    testX, testY = test
+
+
     test = load_data_predict(vocabulary_word2idx, label2idx, questionid_question_lists)
     testX = []
     question_id_list = []
--- taste_classification/textcnn_train.py
+++ taste_classification/textcnn_train.py
@@ -46,16 +46,16 @@ tf.flags.DEFINE_string("cache_file_pickle", cache_file_pickle, "path of vocabula
 
 tf.flags.DEFINE_integer("label_size", 4, "number of label")
 tf.flags.DEFINE_float("learning_rate", 0.01, "learning rate")
-tf.flags.DEFINE_integer("batch_size", 256, "batch size for training/evaluating")  # 批处理的大小 32-->128
+tf.flags.DEFINE_integer("batch_size", 512, "batch size for training/evaluating")  # 批处理的大小 32-->128
 tf.flags.DEFINE_integer("decay_steps", 20000, "how many steps before decay learning rate")
 tf.flags.DEFINE_float("decay_rate", 0.96, "Rate of decay for learning rate")  # 一次衰减多少
 tf.flags.DEFINE_integer("num_sampled", 100, "number of noise sampling")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "to control the activation level of neurons")
 tf.flags.DEFINE_string("ckpt_dir", model_checkpoint, "checkpoint location for the model")
-tf.flags.DEFINE_integer("sentence_len", 200, "max sentence length")
+tf.flags.DEFINE_integer("sentence_len", 500, "max sentence length")
 tf.flags.DEFINE_integer("embed_size", 300, "embedding size")
 tf.flags.DEFINE_boolean("is_training", True, "true:training, false:testing/inference")
-tf.flags.DEFINE_integer("num_epochs", 15, "epoch times")
+tf.flags.DEFINE_integer("num_epochs", 10, "epoch times")
 tf.flags.DEFINE_integer("validate_every", 1, "validate every validate_every epochs")  # 每1轮做一次验证
 tf.flags.DEFINE_boolean("use_embedding", True, "whether to use embedding or not")
 tf.flags.DEFINE_integer("num_filters", 128, "number of filters")
