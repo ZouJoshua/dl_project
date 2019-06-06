88bee737028ddefa91e98f973cbfb7fa6dee36bc
zs <joshua_zou@163.com>
2019-06-06 18:42:03 +0800

    update project

---
+++ tf_model/bilstm_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Joshua
+@Time    : 19-6-6 下午2:20
+@File    : bilstm_model.py
+@Desc    : 
+"""
---
+++ tf_model/textlstm_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Joshua
+@Time    : 19-6-6 下午2:16
+@File    : textlstm_model.py
+@Desc    : 
+"""
