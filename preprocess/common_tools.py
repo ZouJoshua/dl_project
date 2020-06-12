015d08cd0e0571b85236c03188a5d6eb1460949b
zoushuai <11121175@vivo.com>
2020-06-12 18:54:03 +0800

    update project

--- preprocess/common_tools.py
+++ preprocess/common_tools.py
@@ -220,13 +220,94 @@ def get_ngrams(sentence, n_gram=3):
 
 
 class CleanDoc(object):
+    """
+    文本清洗并进行特征抽取
+    """
+    def __init__(self, sentence, language="cn"):
+        if language == "cn":
+            # text = self.clean_cn_text(sentence)
+            clean_text = self.clean_cn_text_by_third_party(sentence)
+            self.char_feature = self.get_cn_char_feature(clean_text)
+            self.token_feature = self.get_cn_token_feature(clean_text)
+        else:
+            self.text = self.clean_en_text(sentence)
 
-    def __init__(self, text):
-        self.text = self.clean_text(text)
-
-    def clean_text(self, text):
+    def get_cn_char_feature(self, text):
         """
-        清洗流程
+        按字切分句子,去除非中文字符及标点，获取char级别特征
+        todo:针对中文字级别处理（针对英文、数字等符号特殊处理）
+        :param text:
+        :return:
+        """
+        # print("splitting chinese char")
+        seg_list = list()
+        none_chinese = ""
+        for char in text:
+            if self.is_chinese(char) is False:
+                if char in self.punc_list:
+                    continue
+                none_chinese += char
+            else:
+                if none_chinese:
+                    seg_list.append(none_chinese)
+                    none_chinese = ""
+                seg_list.append(char)
+        if not seg_list:
+            seg_list = list()
+        return seg_list
+
+    def get_cn_token_feature(self, text):
+        """
+        按结巴分词.去除标点，获取文本token级别特征
+        :param text:
+        :return:
+        """
+        # 精确模式
+        seg_list = jieba.cut(text, cut_all=False)
+        words = [w for w in seg_list if w not in self.punc_list]
+        if not words:
+            words = list()
+
+        return words
+
+    def clean_cn_text(self, sentence):
+        """
+        中文文本清洗流程
+        step1 -> 替换掉换行符、制表符等
+        step2 -> 清洗网址
+        step3 -> 清洗邮箱
+        step4 -> 清洗表情等非英文字符
+        step5 -> 替换多个空格为一个空格
+        :param sentence: 原始文本
+        :return: 清洗后的文本
+        """
+
+        _text = sentence.replace('\u2028', '').replace('\n', '').replace('\t', '')
+        re_h = re.compile('<(/?\w+|!--|!DOCTYPE|\?xml)[^>]*>')
+        _text = re_h.sub('', _text)  # html处理
+        no_html = self.clean_url(_text)
+        no_mail = self.clean_mail(no_html)
+        no_emoji = self.remove_emoji(no_mail)
+        _text = re.sub(r"\s+", " ", no_emoji)
+        return sentence
+
+
+    def clean_cn_text_by_third_party(self, sentence):
+        """
+        用第三方库清洗中文文本
+        """
+        from harvesttext import HarvestText
+        ht_obj = HarvestText()
+        # 去掉微博的@，表情符；网址；email；html代码中的一类的特殊字符等
+        _text = sentence.replace('\u2028', '').replace('\n', '').replace('\t', '')
+        re_h = re.compile('<(/?\w+|!--|!DOCTYPE|\?xml)[^>]*>')
+        _text = re_h.sub('', _text)  # html处理
+        clean_text = ht_obj.clean_text(_text)
+        return clean_text
+
+    def clean_en_text(self, sentence):
+        """
+        英文文本清洗流程
         step1 -> 替换掉换行符、制表符等
         step2 -> 转小写
         step3 -> 清洗网址
@@ -234,26 +315,43 @@ class CleanDoc(object):
         step5 -> 清洗表情等非英文字符
         step6 -> 清洗标点符号、数字
         step7 -> 替换多个空格为一个空格
-        :param text: 原始文本
+        :param sentence: 原始文本
         :return: 清洗后的文本
         """
-        text = text.replace("\r", " ").replace("\n", " ").replace("\t", " ")
+        text = sentence.replace("\r", " ").replace("\n", " ").replace("\t", " ")
         _text = text.lower()
-        no_html = self.clean_html(_text)
+        no_html = self.clean_url(_text)
         no_mail = self.clean_mail(no_html)
         no_emoji = self.remove_emoji(no_mail)
         no_symbol = self.remove_symbol(no_emoji)
         text = re.sub(r"\s+", " ", no_symbol)
         return text
 
-    def remove_en_emoji(self, text):
+    @property
+    def punc_list(self):
+        add_punc = '，。、【 】 “”：；（）《》‘’{}？！⑦()、%^>℃：”“^-——=&#@￥\n「」…『』\u3000\xa0'
+        return string.punctuation + add_punc
+
+    @staticmethod
+    def remove_en_emoji(text):
+        """
+        去除英文表情符号
+        :param text:
+        :return:
+        """
         cleaned_text = ""
         for c in text:
             if (ord(c) >= 65 and ord(c) <= 126) or (ord(c) >= 32 and ord(c) <= 63):
                 cleaned_text += c
         return cleaned_text
 
-    def remove_emoji(self, text):
+    @staticmethod
+    def remove_emoji(text):
+        """
+        去除表情符号
+        :param text:
+        :return:
+        """
         token_list = text.replace("¡", "").replace("¿", "").split(" ")
         em_str = r":.*?:"
         em_p = re.compile(em_str, flags=0)
@@ -269,7 +367,18 @@ class CleanDoc(object):
         cleaned_text = " ".join(clean_token)
         return cleaned_text.strip()
 
-    def clean_html(self, text):
+    @staticmethod
+    def is_chinese(uchar):
+        """
+        判断一个unicode是否是汉字
+        """
+        if (uchar >= u'\u4e00') and (uchar <= u'\u9fa5'):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def clean_url(text):
         """
         去除网址
         1.完整网址https开头的
@@ -287,8 +396,13 @@ class CleanDoc(object):
         text = pattern.sub("", text)
         return text.replace("( )", " ")
 
-    def clean_mail(self, text):
-        # 去除邮箱
+    @staticmethod
+    def clean_mail(text):
+        """
+        去除邮箱
+        :param text:
+        :return:
+        """
         pattern = re.compile(r"\w+[-_.]*[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}")
         text = pattern.sub(" ", text)
         # mail_list = re.findall(pattern, text)
@@ -296,18 +410,29 @@ class CleanDoc(object):
         #     text = text.replace(mail, " ")
         return text
 
-    def remove_symbol_and_digits(self, text):
+    @staticmethod
+    def remove_symbol_and_digits(text):
+        """
+        去除标点符号和数字
+        :param text:
+        :return:
+        """
         del_symbol = string.punctuation + string.digits  # ASCII 标点符号，数字
         remove_punctuation_map = dict((ord(char), " ") for char in del_symbol)
         text = text.translate(remove_punctuation_map)  # 去掉ASCII 标点符号
         return text
 
-    def remove_symbol(self, text):
+    @staticmethod
+    def remove_symbol(text):
+        """
+        去除标点符号
+        :param text:
+        :return:
+        """
         del_symbol = string.punctuation  # ASCII 标点符号
         remove_punctuation_map = dict((ord(char), " ") for char in del_symbol)
         text = text.translate(remove_punctuation_map)  # 去掉ASCII 标点符号
         return text
-
 
 def clean_string(text):
     # 去除网址和邮箱
